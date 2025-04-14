cdcb379c94e999d48d4cef425e8e6d69b1ab4c42
Sai2475 <saisanjay3000@gmail.com>
2025-04-14 11:25:39 +0530

    updated version

--- virtual_painter.py
+++ virtual_painter.py
@@ -1,84 +1,103 @@
 import cv2
 import numpy as np
-import time
 import os
-
-from numpy.distutils.conv_template import header
-
 import Handtracking_module as htm
 
-folderpath = "header"
-myList = os.listdir(folderpath)
+#######################
+brushThickness = 25
+eraserThickness = 100
+########################
+
+folderPath = "Header"
+myList = os.listdir(folderPath)
 print(myList)
+overlayList = []
+for imPath in myList:
+    image = cv2.imread(f'{folderPath}/{imPath}')
+    image = cv2.resize(image, (1280, 125))  # Resize all headers to fit canvas
+    overlayList.append(image)
 
-overlaylist = []
-for imgpath in myList:
-    image = cv2.imread(f"{folderpath}/{imgpath}")
-    image = cv2.resize(image, (1280, 90))  # Resize header to match frame width
-    overlaylist.append(image)
-print(len(overlaylist))
+print(len(overlayList))
+header = overlayList[0]
+drawColor = (255, 0, 255)
 
-Header = overlaylist[0]
+cap = cv2.VideoCapture(1)
+cap.set(3, 1280)
+cap.set(4, 720)
 
-cap = cv2.VideoCapture(0)
-cap.set(3, 1280)  # Frame width
-cap.set(4, 720)   # Frame height
-
-detector = htm.handDetector(detectionCon=0.85)
+detector = htm.handDetector(detectionCon=0.65, maxHands=1)
+xp, yp = 0, 0
+imgCanvas = np.zeros((720, 1280, 3), np.uint8)
 
 while True:
-    #import image
+
+    # 1. Import image
     success, img = cap.read()
-    img = cv2.flip(img,1)
+    img = cv2.flip(img, 1)
 
-    #hand landmarks
+    # 2. Find Hand Landmarks
     img = detector.findHands(img)
-    lmlist = detector.findPosition(img,draw= False)
+    lmList = detector.findPosition(img, draw=False)
 
-    if lmlist and len(lmlist) > 12:
-        #print(lmlist)
+    if lmList:  # instead of len(lmList) != 0
+        # Tip of index and middle fingers
+        x1, y1 = lmList[8][1:]
+        x2, y2 = lmList[12][1:]
 
-        #tip of the fingers
-        x1,y1 = lmlist[8][1:]
-        x2,y2 = lmlist[12][1:]
+        # 3. Check which fingers are up
+        fingers = detector.fingersUp()
 
-        #check which fingers are up
-        fingers = detector.fingersup()
-        print(fingers)
-
-        #selection mode = two fingers
+        # 4. If Selection Mode - Two fingers up
         if fingers[1] and fingers[2]:
-            cv2.rectangle(img, (x1,y1-25),(x2,y2 + 25),(255,0,255),cv2.FILLED)
-            print("selection mode")
+            print("Selection Mode")
             if y1 < 125:
                 if 250 < x1 < 450:
-                    header = overlaylist[0]
-                elif 550 <x1 <750:
-                    header = overlaylist[1]
-                elif 800 <x1 <950:
-                    header = overlaylist[2]
-                elif 1050 <x1 <1200:
-                    header = overlaylist[1]
+                    header = overlayList[0]
+                    drawColor = (255, 0, 255)
+                elif 550 < x1 < 750:
+                    header = overlayList[1]
+                    drawColor = (255, 0, 0)
+                elif 800 < x1 < 950:
+                    header = overlayList[2]
+                    drawColor = (0, 255, 0)
+                elif 1050 < x1 < 1200:
+                    header = overlayList[3]
+                    drawColor = (0, 0, 0)
+            cv2.rectangle(img, (x1, y1 - 25), (x2, y2 + 25), drawColor, cv2.FILLED)
 
-        #drawing mode = one finger
-        if fingers[1] and fingers[2] == False:
-            cv2.circle(img,(x1,y1),15,(255,0,255),cv2.FILLED)
-            print("drawing mode")
+        # 5. If Drawing Mode - Only index finger up
+        elif fingers[1] and not fingers[2]:
+            print("Drawing Mode")
+            cv2.circle(img, (x1, y1), 15, drawColor, cv2.FILLED)
 
+            if xp == 0 and yp == 0:
+                xp, yp = x1, y1
 
+            # Drawing logic
+            if drawColor == (0, 0, 0):
+                cv2.line(img, (xp, yp), (x1, y1), drawColor, eraserThickness)
+                cv2.line(imgCanvas, (xp, yp), (x1, y1), drawColor, eraserThickness)
+            else:
+                cv2.line(img, (xp, yp), (x1, y1), drawColor, brushThickness)
+                cv2.line(imgCanvas, (xp, yp), (x1, y1), drawColor, brushThickness)
 
+            xp, yp = x1, y1
 
+        # Clear Canvas when all fingers are up
+        if all(fingers):
+            imgCanvas = np.zeros((720, 1280, 3), np.uint8)
 
+    # Merge canvas with camera feed
+    imgGray = cv2.cvtColor(imgCanvas, cv2.COLOR_BGR2GRAY)
+    _, imgInv = cv2.threshold(imgGray, 50, 255, cv2.THRESH_BINARY_INV)
+    imgInv = cv2.cvtColor(imgInv, cv2.COLOR_GRAY2BGR)
+    img = cv2.bitwise_and(img, imgInv)
+    img = cv2.bitwise_or(img, imgCanvas)
 
-    #setting the header images
-    if not success:
-        break  # Exit loop if the frame is not captured
-
-    img[0:90, 0:1280] = Header  # Assign resized header to frame
+    # 6. Overlay the header
+    img[0:125, 0:1280] = header
 
     cv2.imshow("Image", img)
-    if cv2.waitKey(1) & 0xFF == ord('q'):  # Press 'q' to exit
-        break
-
-cap.release()
-cv2.destroyAllWindows()
+    cv2.imshow("Canvas", imgCanvas)
+    cv2.imshow("Inv", imgInv)
+    cv2.waitKey(1)
