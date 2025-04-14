ccc2ff9e7614d62bdb97f7e0e9e72c0ab6c8c515
Sai2475 <saisanjay3000@gmail.com>
2025-04-14 11:10:24 +0530

    updated

--- Handtracking_module.py
+++ Handtracking_module.py
@@ -17,17 +17,18 @@ class handDetector():
         self.mpDraw = mp.solutions.drawing_utils
         self.tipIds = [4, 8, 12, 16, 20]
 
-    def findHands(self, img, draw=True):
-        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        self.results = self.hands.process(imgRGB)
+        def findHands(self, img, draw=True):
+            imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            self.results = self.hands.process(imgRGB)
+            # print(results.multi_hand_landmarks)
 
-        if self.results.multi_hand_landmarks:
-            for handLms in self.results.multi_hand_landmarks:
-                if draw:
-                    self.mpDraw.draw_landmarks(img, handLms,
-                                               self.mpHands.HAND_CONNECTIONS)
+            if self.results.multi_hand_landmarks:
+                for handLms in self.results.multi_hand_landmarks:
+                    if draw:
+                        self.mpDraw.draw_landmarks(img, handLms,
+                                                   self.mpHands.HAND_CONNECTIONS)
 
-        return img
+            return img
 
         def findPosition(self, img, handNo=0, draw=True):
             self.lmList = []
