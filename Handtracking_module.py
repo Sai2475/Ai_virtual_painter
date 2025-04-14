0190587d686b0bef6fa687d7fbaecf063f8cb73c
Sai2475 <saisanjay3000@gmail.com>
2025-04-14 11:11:56 +0530

    updated

--- Handtracking_module.py
+++ Handtracking_module.py
@@ -31,23 +31,23 @@ class handDetector():
             return img
 
         def findPosition(self, img, handNo=0, draw=True):
+            xList = []
+            yList = []
+            bbox = []
             self.lmList = []
-            xList, yList = [], []
+            if self.results.multi_hand_landmarks:
+                myHand = self.results.multi_hand_landmarks[handNo]
+                for id, lm in enumerate(myHand.landmark):
+                    # print(id, lm)
+                    h, w, c = img.shape
+                    cx, cy = int(lm.x * w), int(lm.y * h)
+                    xList.append(cx)
+                    yList.append(cy)
+                    # print(id, cx, cy)
+                    self.lmList.append([id, cx, cy])
+                    if draw:
+                        cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
 
-        if self.results.multi_hand_landmarks:
-            myHand = self.results.multi_hand_landmarks[handNo]
-            for id, lm in enumerate(myHand.landmark):
-                h, w, c = img.shape
-                cx, cy = int(lm.x * w), int(lm.y * h)
-                xList.append(cx)
-                yList.append(cy)
-                self.lmList.append([id, cx, cy])
-                if draw:
-                    cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
-
-        bbox = None
-
-        if xList and yList:  # Ensure lists are not empty
             xmin, xmax = min(xList), max(xList)
             ymin, ymax = min(yList), max(yList)
             bbox = xmin, ymin, xmax, ymax
@@ -55,10 +55,8 @@ class handDetector():
             if draw:
                 cv2.rectangle(img, (xmin - 20, ymin - 20), (xmax + 20, ymax + 20),
                               (0, 255, 0), 2)
-        else:
-            bbox = None
 
-        return self.lmList, bbox
+            return self.lmList, bbox
 
     def fingersUp(self):
         fingers = []
